Fixed models (removed softmax), added Adam as opt and updateds FTDD

models/unimodal_models.py
@@ -32,9 +32,7 @@ class VGG_Like(nn.Module):
         self.fc2 = nn.Linear(32, 8)
         self.dropout2 = nn.Dropout1d(dropout_rate)
 
-        self.fc3 = nn.Linear(8, num_classes)
-        self.dropout3 = nn.Dropout1d(dropout_rate)
-        self.softmax = nn.Softmax(dim=1)
+        self.fc3 = nn.Linear(32, num_classes)
 
     def forward(self, x):
         x = F.relu(self.conv1_1(x))
@@ -53,13 +51,10 @@ class VGG_Like(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.dropout1(x)
 
-        x = F.relu(self.fc2(x))
-        x = self.dropout2(x)
+        # x = F.relu(self.fc2(x))
+        # x = self.dropout2(x)
 
-        x = F.relu(self.fc3(x))
-        x = self.dropout3(x)
-
-        x = self.softmax(x)
+        x = self.fc3(x)
 
         return x
 
@@ -73,7 +68,6 @@ class LeNet_Like(nn.Module):
         self.fc1 = nn.Linear(16*13*13, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, num_classes)
-        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -81,7 +75,7 @@ class LeNet_Like(nn.Module):
         x = x.view(-1, 16*13*13)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.softmax(self.fc3(x))
+        x = self.fc3(x)
         return x
     
 class LeNet_Like1D(nn.Module):
@@ -93,7 +87,6 @@ class LeNet_Like1D(nn.Module):
         self.fc1 = nn.Linear(16*9, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, num_classes)
-        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -101,6 +94,6 @@ class LeNet_Like1D(nn.Module):
         x = x.view(-1, 16*9)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.softmax(self.fc3(x))
+        x = self.fc3(x)
         return x
 

train.py
@@ -62,8 +62,14 @@ class Trainer():
 
         # initialize loss and optimizer
         self.loss_object = nn.CrossEntropyLoss()
-        self.optimizer = optim.SGD(self.model.parameters(
-        ), lr=config_dict["lr"], momentum=config_dict["momentum"])
+        if self.config_dict["optimizer"].lower() == "adam":
+            self.optimizer = optim.Adam(
+                self.model.parameters(), lr=config_dict["lr"])
+        elif self.config_dict["optimizer"].lower() == "sgd":
+            self.optimizer = optim.SGD(self.model.parameters(
+            ), lr=config_dict["lr"], momentum=config_dict["momentum"])
+        else:
+            raise Exception(f"The optimizer '{self.config_dict['optimizer']}' is not supported!")
 
         # initialize metrics
         self.train_confusion_matrix = ConfusionMatrix(
@@ -254,9 +260,10 @@ class Trainer():
         """
         current_train_acc = self.train_confusion_matrix.get_accuracy()
         if current_train_acc < 0.6 and not force_save:
-            logging.info(f"State_dict not save, as accuracy for training dict is below 60% = not worth it")
+            logging.info(
+                f"State_dict not save, as accuracy for training dict is below 60% = not worth it")
             return
-    
+
         # create save path
         save_path = os.path.join(
             self.run_paths_dict["model_ckpts"], f"{self.model._get_name()}_{suffix_for_filename}.pt")
@@ -264,4 +271,5 @@ class Trainer():
         # save the current state_dict of the model to save_path
         torch.save(self.model.state_dict(), save_path)
 
-        logging.info(f"Stored current state_dict of the model to '{save_path}'")
+        logging.info(
+            f"Stored current state_dict of the model to '{save_path}'")