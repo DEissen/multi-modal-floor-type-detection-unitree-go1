8bba5aba166652243ca9a527f2a25427ed44de13
st177975 <st177975@stud.uni-stuttgart.de>
2024-01-25 15:51:46 +0100

    Formatted and updated main for separate test set

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from visualization.visualization import visualize_data_sample_or_batch, visualiz
 from custom_utils.custom_utils import gen_run_dir, CustomLogger, store_used_config
 
 
-def main(perform_training=True, sensors=None, run_path=r"", num_ckpt_to_load=None, logger=None):
+def main(perform_training=True, sensors=None, run_path=r"", num_ckpt_to_load=None, logger=None, test_dataset_path=None):
     """
         Main function for training and evaluation of unimodal and multimodal models for the FTD dataset.
         Most important config parameters must be changed in the function itself, as this function is the core of the framework.
@@ -26,10 +26,12 @@ def main(perform_training=True, sensors=None, run_path=r"", num_ckpt_to_load=Non
             - run_path (str): Path to files stored from a previous run for loading a checkpoint, ...
             - num_ckpt_to_load (int): Number of the checkpoint to restore from run_path. If None is provided, the last element of the sorted file list will be taken.
             - logger (CustomLogger): Instance of custom logger class which must be provided in case main() is called multiple times by another function/program to prevent multiple logging
+            - test_dataset_path (str): Path to the files of the separate test dataset (default = None).
+                                       If None is provided, the training set will be randomly split in test and training set.
     """
     # ####### configurable parameters #######
     # ### variables for dataset config
-    dataset_path = r"C:\Users\Dominik\Downloads\FTDD1.1_preprocessed"
+    dataset_path = r"D:\MA_Daten\FTDD2.0_preprocessed\FTDD_2.0_train"
     mapping_filename = "label_mapping_full_dataset.json"
     preprocessing_config_filename = "preprocessing_config.json"
     faulty_data_creation_config_filename = "faulty_data_creation_config.json"
@@ -38,7 +40,8 @@ def main(perform_training=True, sensors=None, run_path=r"", num_ckpt_to_load=Non
     # List of all sensors available: 'accelerometer', 'BellyCamLeft', 'BellyCamRight', 'bodyHeight', 'ChinCamLeft', 'ChinCamRight', 'footForce', 'gyroscope',
     #                                'HeadCamLeft', 'HeadCamRight', 'LeftCamLeft', 'LeftCamRight', 'mode', 'rpy', 'RightCamLeft', 'RightCamRight', 'velocity', 'yawSpeed'
     if sensors == None:
-        sensors = ['accelerometer', 'BellyCamLeft', 'BellyCamRight', 'bodyHeight', 'ChinCamLeft']
+        sensors = ['accelerometer', 'BellyCamLeft',
+                   'BellyCamRight', 'bodyHeight', 'ChinCamLeft']
 
     # ### config for training
     train_config_dict = {
@@ -64,26 +67,31 @@ def main(perform_training=True, sensors=None, run_path=r"", num_ckpt_to_load=Non
 
     logger.start_logger(run_paths_dict["logs_path"], stream_log=True)
 
-    # ####### prepare dataset and load config #######
-    # create dataset
+    # ####### prepare datasets #######
+    # load datasets
     transformed_dataset = FloorTypeDetectionDataset(
         dataset_path, sensors, mapping_filename, preprocessing_config_filename, faulty_data_creation_config_filename)
+    # if path to training dataset is provide it load it separately and use complete transformed dataset as training set
+    if test_dataset_path != None:
+        ds_train = transformed_dataset
+        ds_test = FloorTypeDetectionDataset(
+            test_dataset_path, sensors, mapping_filename, preprocessing_config_filename, faulty_data_creation_config_filename)
+    # else split the transformed dataset randomly in a test and train set depending on the use case
+    else:
+        if perform_training:
+            # if perform_training == True use 10 % as a test set
+            train_size = int(0.9 * len(transformed_dataset))
+        else:
+            # use whole dataset for evaluation in case of preform_training == False
+            train_size = 0
 
-    # get all possible config dicts for logging
+        test_size = len(transformed_dataset) - train_size
+        ds_train, ds_test = torch.utils.data.random_split(
+            transformed_dataset, [train_size, test_size])
+
+    # ####### get all config dicts for logging #######
     label_mapping_dict = transformed_dataset.get_mapping_dict()
     preprocessing_config_dict = transformed_dataset.get_preprocessing_config()
-
-    # split in train and test dataset
-    if perform_training:
-        train_size = int(0.9 * len(transformed_dataset))
-    else:
-        # current implementation: use whole dataset for evaluation in case of preform_training == False
-        # Idea: Use different dataset from other measurements for testing
-        train_size = 0
-
-    test_size = len(transformed_dataset) - train_size
-    ds_train, ds_test = torch.utils.data.random_split(
-        transformed_dataset, [train_size, test_size])
 
     # ####### define model (automatically select uni or multimodal model) #######
     # ## multimodal model when more than 1 sensor is provided
@@ -103,7 +111,8 @@ def main(perform_training=True, sensors=None, run_path=r"", num_ckpt_to_load=Non
     # ## unimodal model for images if "Cam" in the sensor name
     elif "Cam" in sensors[0]:
         # for images
-        model = LeNet_Like(train_config_dict["num_classes"], train_config_dict["dropout_rate"])
+        model = LeNet_Like(
+            train_config_dict["num_classes"], train_config_dict["dropout_rate"])
         # model = VGG_Like(train_config_dict["num_classes"], train_config_dict["dropout_rate"])
 
     # ## unimodal model for timeseries data in remaining case
@@ -151,30 +160,31 @@ def main(perform_training=True, sensors=None, run_path=r"", num_ckpt_to_load=Non
 
 
 def complete_unimodal_test():
-    perform_training=True
-    run_path=r""
-    num_ckpt_to_load=None
+    perform_training = True
+    run_path = r""
+    num_ckpt_to_load = None
     logger = CustomLogger()
 
     # list of sensors to use
     sensors = ['accelerometer', 'BellyCamLeft', 'BellyCamRight', 'bodyHeight', 'ChinCamLeft', 'ChinCamRight', 'footForce', 'gyroscope',
-            'HeadCamLeft', 'HeadCamRight', 'LeftCamLeft', 'LeftCamRight', 'mode', 'RightCamLeft', 'RightCamRight', 'rpy', 'velocity', 'yawSpeed']
+               'HeadCamLeft', 'HeadCamRight', 'LeftCamLeft', 'LeftCamRight', 'mode', 'RightCamLeft', 'RightCamRight', 'rpy', 'velocity', 'yawSpeed']
 
     for sensor in sensors:
         sensor = [sensor]
         main(perform_training, sensor, run_path, num_ckpt_to_load, logger)
 
+
 def test_random_multimodal_models(number_of_runs):
-    perform_training=True
-    run_path=r""
-    num_ckpt_to_load=None
+    perform_training = True
+    run_path = r""
+    num_ckpt_to_load = None
     logger = CustomLogger()
 
     # run training for number_of_runs times with random subset of sensors
     for i in range(number_of_runs):
         # reinitialize list for each run
         sensors = ['accelerometer', 'BellyCamLeft', 'BellyCamRight', 'bodyHeight', 'ChinCamLeft', 'ChinCamRight', 'footForce', 'gyroscope',
-                'HeadCamLeft', 'HeadCamRight', 'LeftCamLeft', 'LeftCamRight', 'mode', 'RightCamLeft', 'RightCamRight', 'rpy', 'velocity', 'yawSpeed']
+                   'HeadCamLeft', 'HeadCamRight', 'LeftCamLeft', 'LeftCamRight', 'mode', 'RightCamLeft', 'RightCamRight', 'rpy', 'velocity', 'yawSpeed']
         # shuffle full list
         shuffle(sensors)
         # get random number of sensors (at least 2 and max 1/3 of sensors to keep it short)
@@ -186,6 +196,6 @@ def test_random_multimodal_models(number_of_runs):
 
 if __name__ == "__main__":
     # ### uncomment function which you want to use (default is main())
-    # main()
-    complete_unimodal_test()
+    main()
+    # complete_unimodal_test()
     # test_random_multimodal_models(10)
